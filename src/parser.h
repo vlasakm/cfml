babff94f4a769a2b5a3260f7cc521fa8eeb6a8b2
Michal Vlasák <vlasami6@fit.cvut.cz>
2023-02-18 01:59:06 +0100

    Add public API documentation

--- src/parser.h
+++ src/parser.h
@@ -10,6 +10,11 @@
 
 #include "arena.h"
 
+// See the bottom of this file for parser API.
+
+// This header mainly defines the AST emitted by the reference FML parser and
+// a few utility types.
+
 typedef uint8_t  u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
@@ -20,11 +25,16 @@ typedef int16_t i16;
 typedef int32_t i32;
 typedef int64_t i64;
 
+// `Str` is a pair of pointer and length and represents an immutable string. It
+// can be constructed from C string literals with the below `STR` macro.
 typedef struct {
 	const u8 *str;
 	size_t len;
 } Str;
 #define STR(lit) (Str) { .str = (const u8 *) lit, .len = sizeof(lit) - 1 }
+
+// See https://courses.fit.cvut.cz/NI-RUN/specs/ast.html for details about the
+// AST.
 
 typedef enum {
 	AST_NULL,
@@ -177,6 +187,21 @@ typedef struct {
 	size_t expression_cnt;
 } AstTop;
 
+// Parser takes a source code (`Str`) as input and produces AST (`Ast`) as an
+// output. The function `parse` is the most general way of using the parser. It
+// receives an `Arena` in which the `Ast` will be allocated (the user is
+// responsible for deallocation) and `GArena` which will be used as scratch
+// space (parser may allocate in it, but returns it to the previous state).
+//
+// In case of any encountered errors, `NULL` is ultimately returned, errors are
+// reported to the caller with a callback function and the space allocated in
+// the `Arena` by the parser is deallocated (restored to the state at the start
+// of parsing).
+//
+// For simplicity, the `parse_src` function can be used instead. It uses the
+// predefined example `parser_error_cb` callback, which is defined to report
+// errors to standard error (see it also for details about the callback
+// behavior).
 
 Ast *parse_src(Arena *arena, Str source);
 
